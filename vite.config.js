3d265a22cb1f003d5f7333c3f6227ec1d619ca81
vehep585-dev <vehep585@gmail.com>
2026-06-03 14:37:11 +0500

    app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+
+      manifest: false,
+
+      includeAssets: [
+        "favicon.ico",
+        "favicon.svg",
+        "apple-touch-icon.png",
+        "icon-192.png",
+        "logo-512.png",
+        "og-banner.jpg",
+      ],
+
+      workbox: {
+         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        globPatterns: ["**/*.{js,css,html,svg,png,jpg,jpeg,webp,woff2,json}"],
+        navigateFallback: "/index.html",
+        runtimeCaching: [
+          // your caching rules here
+        ],
+      },
+
+      devOptions: {
+        enabled: true,
+        type: "module",
+      },
+    }),
+  ],
+
   server: {
     port: 3000,
   },
